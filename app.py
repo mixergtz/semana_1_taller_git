63526f0c0a8ca6739b2fbbb2fd938af1042d694e
Mixer Gutierrez <mixergutierrez@gmail.com>
2025-08-09 13:56:45 -0500

    Agregar comentario

--- app.py
+++ app.py
@@ -242,4 +242,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True) # run_server is deprecated
